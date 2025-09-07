88a88a82143221bcd55096eee86877e8cf5fcc6b
am4513-blip <am4513@protonmail.com>
2025-09-07 10:43:21 -0700

    chngd pos of str_wheel icon and veh speed

--- selfdrive/ui/qt/onroad/hud.cc
+++ selfdrive/ui/qt/onroad/hud.cc
@@ -97,10 +97,10 @@ void HudRenderer::drawCurrentSpeed(QPainter &p, const QRect &surface_rect) {
   QString speedStr = QString::number(std::nearbyint(speed));
 
   p.setFont(InterFont(176, QFont::Bold));
-  drawText(p, surface_rect.center().x(), 1000, speedStr); //y = 210 orig
+  drawText(p, surface_rect.center().x(), 710, speedStr); //y = 210 orig
 
   p.setFont(InterFont(66));
-  drawText(p, surface_rect.center().x(), 1080, is_metric ? tr("km/h") : tr("mph"), 200);  //y = 290 orig
+  drawText(p, surface_rect.center().x(), 790, is_metric ? tr("km/h") : tr("mph"), 200);  //y = 290 orig
 }
 
 void HudRenderer::drawText(QPainter &p, int x, int y, const QString &text, int alpha) {
